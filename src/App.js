67b12ff100a13f076e5ee4f1b3254c64b34fca75
Brian Kent <bkent@thewellnessnetwork.net>
2020-02-12 16:52:49 -0600

    test

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from "./components/Home";
 function App() {
   return (
     <div className="App">
+      Brian2
       <BrowserRouter>
             <Route path="/app" component={Home} />
             <Route path="/" component={Launcher} exact />
