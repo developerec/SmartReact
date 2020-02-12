01447496936add0a28ea3e65e39047214cca9da2
Brian Kent <bkent@thewellnessnetwork.net>
2020-02-12 15:55:19 -0600

    Initial coding

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import FhirClientProvider from "./FhirClientProvider";
+//import Chart from "./Chart";
+import Patient from "./Patient";
+
+/**
+ * Wraps everything into `FhirClientProvider` so that any component
+ * can have access to the fhir client through the context.
+ */
+export default function Page() {
+    return (
+        <FhirClientProvider>
+            <Patient />
+            <hr />
+        </FhirClientProvider>
+    );
+}
